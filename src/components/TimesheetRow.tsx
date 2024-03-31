404aba32788d3e0fa8f7ebe1516586fd9f46cb89
Jacobtread <jacobtread@gmail.com>
2024-03-31 20:26:12 +1300

    feat: improved editing experience

--- src/components/TimesheetRow.tsx
+++ src/components/TimesheetRow.tsx
@@ -1,56 +1,30 @@
-import React, { ChangeEvent, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import {
 	isEntryRunning,
 	getEntryDuration,
-	removeEntry,
 	updateEntry,
 	withSubEntry,
 	isKeepRunning,
 } from "@/timekeep";
 import { useSettings } from "@/hooks/use-settings-context";
-import {
-	CheckIcon,
-	ChevronRight,
-	EditIcon,
-	PlayIcon,
-	TrashIcon,
-} from "lucide-react";
-import {
-	formatDuration,
-	formatEditableTimestamp,
-	formatTimestamp,
-	unformatEditableTimestamp,
-} from "src/utils";
+import { EditIcon, PlayIcon } from "lucide-react";
+import { formatDuration, formatTimestamp } from "src/utils";
 import { useTimekeep } from "@/hooks/use-timekeep-context";
 import { TimeEntry } from "@/schema";
 import moment from "moment";
+import TimesheetRowEditing from "@/components/TimesheetRowEditing";
 
 type Props = {
 	entry: TimeEntry;
 	indent: number;
 };
 
-type EditingState = {
-	editing: boolean;
-	name: string;
-	startTime: string;
-	endTime: string;
-};
-
-const DEFAULT_EDITING_STATE = {
-	editing: false,
-	name: "",
-	startTime: "",
-	endTime: "",
-};
-
 export default function TimesheetRow({ entry, indent }: Props) {
 	const settings = useSettings();
 	const isRunning = isEntryRunning(entry);
 	const { setTimekeep, isTimekeepRunning } = useTimekeep();
-	const [editingState, setEditingState] = useState<EditingState>(
-		DEFAULT_EDITING_STATE
-	);
+
+	const [editing, setEditing] = useState(false);
 
 	// Persist the duration to prevent it updating real-time
 	const duration = useMemo(() => {
@@ -80,151 +54,47 @@ export default function TimesheetRow({ entry, indent }: Props) {
 		});
 	};
 
-	const onClickDelete = () => {
-		if (!confirm("Are you sure you want to delete this entry?")) {
-			return;
-		}
-
-		setTimekeep((timekeep) => ({
-			entries: removeEntry(timekeep.entries, entry),
-		}));
-	};
-
-	const onClickEdit = () => {
-		if (editingState.editing) {
-			const newEntry = { ...entry, name: editingState.name };
-
-			// Update the start and end times for non groups
-			if (newEntry.subEntries === null) {
-				const startTime = unformatEditableTimestamp(
-					editingState.startTime,
-					settings
-				);
-				if (startTime.isValid()) {
-					newEntry.startTime = startTime;
-				}
-
-				const endTime = unformatEditableTimestamp(
-					editingState.endTime,
-					settings
-				);
-				if (endTime.isValid()) {
-					newEntry.endTime = endTime;
-				}
-			}
-
-			// Reset the editing state
-			setEditingState(DEFAULT_EDITING_STATE);
-
-			// Save the updated entry
-			setTimekeep((timekeep) => ({
-				entries: updateEntry(timekeep.entries, entry, newEntry),
-			}));
-		} else {
-			// Set the editing state
-			setEditingState({
-				name: entry.name,
-				startTime: entry.startTime
-					? formatEditableTimestamp(entry.startTime, settings)
-					: "",
-				endTime: entry.endTime
-					? formatEditableTimestamp(entry.endTime, settings)
-					: "",
-				editing: true,
-			});
-		}
-	};
-
-	const onChangeName = (event: ChangeEvent<HTMLInputElement>) => {
-		setEditingState((editing) => ({
-			...editing,
-			name: event.target.value,
-		}));
-	};
-
-	const onChangeStartTime = (event: ChangeEvent<HTMLInputElement>) => {
-		setEditingState((editing) => ({
-			...editing,
-			startTime: event.target.value,
-		}));
-	};
-
-	const onChangeEndTime = (event: ChangeEvent<HTMLInputElement>) => {
-		setEditingState((editing) => ({
-			...editing,
-			endTime: event.target.value,
-		}));
-	};
-
-	// Renders the name text (Input when editing)
-	const renderName = editingState.editing ? (
-		<input
-			className="timekeep-input"
-			type="text"
-			value={editingState.name}
-			onChange={onChangeName}
-		/>
-	) : (
-		<span className="timekeep-name" title={entry.name}>
-			{entry.name}
-		</span>
-	);
-
-	// Renders the start time text (Input when editing)
-	const renderStartTime =
-		entry.startTime &&
-		(editingState.editing ? (
-			<input
-				className="timekeep-input"
-				type="text"
-				value={editingState.startTime}
-				onChange={onChangeStartTime}
+	if (editing) {
+		return (
+			<TimesheetRowEditing
+				entry={entry}
+				onFinishEditing={() => setEditing(false)}
 			/>
-		) : (
-			<span className="timekeep-time">
-				{formatTimestamp(entry.startTime, settings)}
-			</span>
-		));
-
-	// Renders the end time text (Input when editing)
-	const renderEndTime =
-		entry.endTime &&
-		(editingState.editing ? (
-			<input
-				type="text"
-				value={editingState.endTime}
-				onChange={onChangeEndTime}
-			/>
-		) : (
-			<span className="timekeep-time">
-				{formatTimestamp(entry.endTime, settings)}
-			</span>
-		));
+		);
+	}
 
 	return (
 		<tr className="timekeep-row">
 			<td className="timekeep-col timekeep-col--name">
-				{!editingState.editing && (
-					<>
-						{Array.from(Array(indent).keys()).map((indent) => (
-							<span
-								style={{
-									display: "inline-block",
-									height: "1rem",
-									width: "0.2rem",
-									backgroundColor: "var(--color-base-40)",
-									marginRight: "0.1rem",
-								}}></span>
-						))}
-					</>
-				)}
+				{Array.from(Array(indent).keys()).map(() => (
+					<span
+						style={{
+							display: "inline-block",
+							height: "1rem",
+							width: "0.2rem",
+							borderLeft: "0.1rem solid var(--color-base-40)",
+							marginRight: "0.1rem",
+						}}></span>
+				))}
 
-				{renderName}
+				<span className="timekeep-name" title={entry.name}>
+					{entry.name}
+				</span>
 			</td>
 			<td className="timekeep-col timekeep-col--time">
-				{renderStartTime}
+				{entry.startTime && (
+					<span className="timekeep-time">
+						{formatTimestamp(entry.startTime, settings)}
+					</span>
+				)}
 			</td>
-			<td className="timekeep-col timekeep-col--time">{renderEndTime}</td>
+			<td className="timekeep-col timekeep-col--time">
+				{entry.endTime && (
+					<span className="timekeep-time">
+						{formatTimestamp(entry.endTime, settings)}
+					</span>
+				)}
+			</td>
 			<td className="timekeep-col timekeep-col--duration">
 				<span className="timekeep-time">{duration}</span>
 			</td>
@@ -238,16 +108,9 @@ export default function TimesheetRow({ entry, indent }: Props) {
 					</button>
 					<button
 						disabled={isRunning || !isEditable}
-						onClick={onClickEdit}
+						onClick={() => setEditing(true)}
 						className="timekeep-action">
-						{editingState.editing ? (
-							<CheckIcon width="1em" height="1em" />
-						) : (
-							<EditIcon width="1em" height="1em" />
-						)}
-					</button>
-					<button onClick={onClickDelete} className="timekeep-action">
-						<TrashIcon width="1em" height="1em" />
+						<EditIcon width="1em" height="1em" />
 					</button>
 				</div>
 			</td>
---
+++ src/components/TimesheetRowEditing.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from "react";
+import { removeEntry, updateEntry } from "@/timekeep";
+import { useSettings } from "@/hooks/use-settings-context";
+import { CheckIcon, TrashIcon, XIcon } from "lucide-react";
+import { unformatEditableTimestamp, formatEditableTimestamp } from "@/utils";
+import { useTimekeep } from "@/hooks/use-timekeep-context";
+import { TimeEntry } from "@/schema";
+
+type Props = {
+	entry: TimeEntry;
+	onFinishEditing: VoidFunction;
+};
+
+export default function TimesheetRowEditing({ entry, onFinishEditing }: Props) {
+	const settings = useSettings();
+	const { setTimekeep } = useTimekeep();
+
+	const [name, setName] = useState(entry.name);
+	const [startTime, setStartTime] = useState("");
+	const [endTime, setEndTime] = useState("");
+
+	useEffect(() => {
+		setName(entry.name);
+		setStartTime(
+			entry.startTime
+				? formatEditableTimestamp(entry.startTime, settings)
+				: ""
+		);
+		setEndTime(
+			entry.endTime
+				? formatEditableTimestamp(entry.endTime, settings)
+				: ""
+		);
+	}, [entry]);
+
+	const onClickDelete = () => {
+		if (!confirm("Are you sure you want to delete this entry?")) {
+			return;
+		}
+
+		setTimekeep((timekeep) => ({
+			entries: removeEntry(timekeep.entries, entry),
+		}));
+	};
+
+	const onClickEdit = () => {
+		const newEntry = { ...entry, name };
+
+		// Update the start and end times for non groups
+		if (newEntry.subEntries === null) {
+			const startTimeValue = unformatEditableTimestamp(
+				startTime,
+				settings
+			);
+			if (startTimeValue.isValid()) {
+				newEntry.startTime = startTimeValue;
+			}
+
+			const endTimeValue = unformatEditableTimestamp(endTime, settings);
+			if (endTimeValue.isValid()) {
+				newEntry.endTime = endTimeValue;
+			}
+		}
+
+		// Save the updated entry
+		setTimekeep((timekeep) => ({
+			entries: updateEntry(timekeep.entries, entry, newEntry),
+		}));
+
+		onFinishEditing();
+	};
+
+	return (
+		<tr>
+			<td colSpan={5}>
+				<div className="timesheet-editing">
+					<label>
+						Name
+						<input
+							className="timekeep-input"
+							type="text"
+							value={name}
+							onChange={(event) => setName(event.target.value)}
+						/>
+					</label>
+					{entry.startTime && (
+						<label>
+							Start Time
+							<input
+								className="timekeep-input"
+								type="text"
+								value={startTime}
+								onChange={(event) =>
+									setStartTime(event.target.value)
+								}
+							/>
+						</label>
+					)}
+					{entry.endTime && (
+						<label>
+							End Time
+							<input
+								className="timekeep-input"
+								type="text"
+								value={endTime}
+								onChange={(event) =>
+									setEndTime(event.target.value)
+								}
+							/>
+						</label>
+					)}
+					<div className="timesheet-editing-actions">
+						<button
+							onClick={onClickEdit}
+							className="timekeep-action">
+							<CheckIcon
+								width="1em"
+								height="1em"
+								style={{ marginRight: "0.25rem" }}
+							/>
+							Save
+						</button>
+						<button
+							onClick={onFinishEditing}
+							className="timekeep-action">
+							<XIcon
+								width="1em"
+								height="1em"
+								style={{ marginRight: "0.25rem" }}
+							/>
+							Cancel
+						</button>
+						<button
+							onClick={onClickDelete}
+							className="timekeep-action timekeep-action--end">
+							<TrashIcon
+								width="1em"
+								height="1em"
+								style={{ marginRight: "0.25rem" }}
+							/>
+							Delete
+						</button>
+					</div>
+				</div>
+			</td>
+		</tr>
+	);
+}
