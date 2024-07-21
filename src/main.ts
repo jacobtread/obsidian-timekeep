6482a16620ebd281b4a5638b3a97d94bf54f0dae
Jacobtread <jacobtread@gmail.com>
2024-07-21 19:43:22 +1200

    refactor: move markdown renderer to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,20 @@
-import App from "@/App";
-import moment, { Moment } from "moment";
-import React, { StrictMode } from "react";
-import { Root, createRoot } from "react-dom/client";
+import { Moment } from "moment";
 import { TimekeepSettingsTab } from "@/settings-tab";
 import { PluginManifest, App as ObsidianApp } from "obsidian";
 import { defaultSettings, TimekeepSettings } from "@/settings";
-import {
-	TFile,
-	Plugin,
-	TAbstractFile,
-	MarkdownRenderChild,
-	MarkdownPostProcessorContext,
-} from "obsidian";
+import { Plugin, MarkdownPostProcessorContext } from "obsidian";
 import {
 	load,
-	LoadResult,
 	isKeepRunning,
 	isEntryRunning,
 	getRunningEntry,
 	getEntryDuration,
 	getTotalDuration,
-	replaceTimekeepCodeblock,
 	extractTimekeepCodeblocks,
 } from "@/timekeep";
 
 import { Timekeep, TimeEntry } from "./schema";
-import { createTimekeepStore } from "./store/timekeep-store";
+import { TimekeepMarkdownView } from "./views/timekeep-markdown-view";
 import { SettingsStore, createSettingsStore } from "./store/settings-store";
 
 export default class TimekeepPlugin extends Plugin {
@@ -65,7 +54,7 @@ export default class TimekeepPlugin extends Plugin {
 				const loadResult = load(source);
 
 				context.addChild(
-					new TimekeepComponent(
+					new TimekeepMarkdownView(
 						el,
 						this.app,
 						this.settingsStore,
@@ -101,163 +90,11 @@ export default class TimekeepPlugin extends Plugin {
 		const newValue = update(this.settingsStore.getSettings());
 
 		this.settingsStore.setSettings(newValue);
+
 		await this.saveData(newValue);
 	}
 
 	async saveSettings(): Promise<void> {
 		await this.saveData(this.settingsStore.getSettings());
-	}
-}
-
-class TimekeepComponent extends MarkdownRenderChild {
-	// Obsidian app instance
-	app: ObsidianApp;
-	// Timekeep settings store
-	settingsStore: SettingsStore;
-	// Markdown context for the current markdown block
-	context: MarkdownPostProcessorContext;
-	// Timekeep load result
-	loadResult: LoadResult;
-	// React root
-	root: Root;
-	// Path to the file the timekeep is within
-	fileSourcePath: string;
-
-	constructor(
-		containerEl: HTMLElement,
-		app: ObsidianApp,
-		settingsStore: SettingsStore,
-		context: MarkdownPostProcessorContext,
-		loadResult: LoadResult
-	) {
-		super(containerEl);
-		this.app = app;
-		this.settingsStore = settingsStore;
-		this.context = context;
-		this.loadResult = loadResult;
-		this.root = createRoot(containerEl);
-
-		// Set initial file path
-		this.fileSourcePath = context.sourcePath;
-	}
-
-	onload(): void {
-		// Hook file renaming to update the file we are saving to if its renamed
-		this.registerEvent(
-			this.app.vault.on(
-				"rename",
-				(file: TAbstractFile, oldName: string) => {
-					if (
-						file instanceof TFile &&
-						oldName == this.fileSourcePath
-					) {
-						this.fileSourcePath = file.path;
-					}
-				}
-			)
-		);
-
-		// Render the react content
-		if (this.loadResult.success) {
-			const timekeep = this.loadResult.timekeep;
-
-			// Create a store for the timekeep state
-			const timekeepStore = createTimekeepStore(
-				timekeep,
-				this.trySave.bind(this)
-			);
-
-			this.root.render(
-				React.createElement(
-					StrictMode,
-					{},
-					React.createElement(App, {
-						app: this.app,
-						timekeepStore,
-						settingsStore: this.settingsStore,
-					})
-				)
-			);
-		} else {
-			this.root.render(
-				React.createElement(
-					"p",
-					{ className: "timekeep-container" },
-					"Failed to load timekeep: " + this.loadResult.error
-				)
-			);
-		}
-	}
-
-	onunload(): void {
-		this.root.unmount();
-	}
-
-	/**
-	 * Attempts to save the file normally, if this fails it also attempts
-	 * to save a fallback file
-	 *
-	 * @param timekeep
-	 * @returns Promise of a boolean indicating weather the save was a success
-	 */
-	async trySave(timekeep: Timekeep): Promise<boolean> {
-		try {
-			await this.save(timekeep);
-
-			return true;
-		} catch (e) {
-			console.error("Failed to save timekeep", e);
-
-			try {
-				this.saveFallback(timekeep);
-			} catch (e) {
-				console.error("Couldn't save timekeep fallback", e);
-			}
-
-			return false;
-		}
-	}
-
-	/**
-	 * Attempts to save the timekeep within the current file
-	 *
-	 * @param timekeep The new timekeep data to save
-	 */
-	async save(timekeep: Timekeep) {
-		const sectionInfo = this.context.getSectionInfo(this.containerEl);
-
-		// Ensure we actually have a section to write to
-		if (sectionInfo === null)
-			throw new Error("Section to write did not exist");
-
-		const file = this.app.vault.getFileByPath(this.fileSourcePath);
-
-		// Ensure the file still exists
-		if (file === null) throw new Error("File no longer exists");
-
-		// Replace the stored timekeep block with the new one
-		await this.app.vault.process(file, (data) => {
-			return replaceTimekeepCodeblock(
-				timekeep,
-				data,
-				sectionInfo.lineStart,
-				sectionInfo.lineEnd
-			);
-		});
-	}
-
-	/**
-	 * Fallback saving incase writing back to the timekeep block fails,
-	 * if writing back fails attempt to write to a backup temporary file
-	 * using the current date time
-	 *
-	 * @param timekeep The timekeep to save
-	 */
-	async saveFallback(timekeep: Timekeep) {
-		// Fallback incase of write failure, attempt to write to another file
-		const backupFileName = `timekeep-write-backup-${moment().format("YYYY-MM-DD HH-mm-ss")}.json`;
-
-		// Write to the backup file
-		this.app.vault.create(backupFileName, JSON.stringify(timekeep));
 	}
 }
---
+++ src/views/timekeep-markdown-view.ts
@@ -0,0 +1,168 @@
+import App from "@/App";
+import moment from "moment";
+import { Timekeep } from "@/schema";
+import React, { StrictMode } from "react";
+import { App as ObsidianApp } from "obsidian";
+import { Root, createRoot } from "react-dom/client";
+import { SettingsStore } from "@/store/settings-store";
+import { createTimekeepStore } from "@/store/timekeep-store";
+import { LoadResult, replaceTimekeepCodeblock } from "@/timekeep";
+import {
+	TFile,
+	TAbstractFile,
+	MarkdownRenderChild,
+	MarkdownPostProcessorContext,
+} from "obsidian";
+
+export class TimekeepMarkdownView extends MarkdownRenderChild {
+	// Obsidian app instance
+	app: ObsidianApp;
+	// Timekeep settings store
+	settingsStore: SettingsStore;
+	// Markdown context for the current markdown block
+	context: MarkdownPostProcessorContext;
+	// Timekeep load result
+	loadResult: LoadResult;
+	// React root
+	root: Root;
+	// Path to the file the timekeep is within
+	fileSourcePath: string;
+
+	constructor(
+		containerEl: HTMLElement,
+		app: ObsidianApp,
+		settingsStore: SettingsStore,
+		context: MarkdownPostProcessorContext,
+		loadResult: LoadResult
+	) {
+		super(containerEl);
+		this.app = app;
+		this.settingsStore = settingsStore;
+		this.context = context;
+		this.loadResult = loadResult;
+		this.root = createRoot(containerEl);
+
+		// Set initial file path
+		this.fileSourcePath = context.sourcePath;
+	}
+
+	onload(): void {
+		// Hook file renaming to update the file we are saving to if its renamed
+		this.registerEvent(
+			this.app.vault.on(
+				"rename",
+				(file: TAbstractFile, oldName: string) => {
+					if (
+						file instanceof TFile &&
+						oldName == this.fileSourcePath
+					) {
+						this.fileSourcePath = file.path;
+					}
+				}
+			)
+		);
+
+		// Render the react content
+		if (this.loadResult.success) {
+			const timekeep = this.loadResult.timekeep;
+
+			// Create a store for the timekeep state
+			const timekeepStore = createTimekeepStore(
+				timekeep,
+				this.trySave.bind(this)
+			);
+
+			this.root.render(
+				React.createElement(
+					StrictMode,
+					{},
+					React.createElement(App, {
+						app: this.app,
+						timekeepStore,
+						settingsStore: this.settingsStore,
+					})
+				)
+			);
+		} else {
+			this.root.render(
+				React.createElement(
+					"p",
+					{ className: "timekeep-container" },
+					"Failed to load timekeep: " + this.loadResult.error
+				)
+			);
+		}
+	}
+
+	onunload(): void {
+		this.root.unmount();
+	}
+
+	/**
+	 * Attempts to save the file normally, if this fails it also attempts
+	 * to save a fallback file
+	 *
+	 * @param timekeep
+	 * @returns Promise of a boolean indicating weather the save was a success
+	 */
+	async trySave(timekeep: Timekeep): Promise<boolean> {
+		try {
+			await this.save(timekeep);
+
+			return true;
+		} catch (e) {
+			console.error("Failed to save timekeep", e);
+
+			try {
+				this.saveFallback(timekeep);
+			} catch (e) {
+				console.error("Couldn't save timekeep fallback", e);
+			}
+
+			return false;
+		}
+	}
+
+	/**
+	 * Attempts to save the timekeep within the current file
+	 *
+	 * @param timekeep The new timekeep data to save
+	 */
+	async save(timekeep: Timekeep) {
+		const sectionInfo = this.context.getSectionInfo(this.containerEl);
+
+		// Ensure we actually have a section to write to
+		if (sectionInfo === null)
+			throw new Error("Section to write did not exist");
+
+		const file = this.app.vault.getFileByPath(this.fileSourcePath);
+
+		// Ensure the file still exists
+		if (file === null) throw new Error("File no longer exists");
+
+		// Replace the stored timekeep block with the new one
+		await this.app.vault.process(file, (data) => {
+			return replaceTimekeepCodeblock(
+				timekeep,
+				data,
+				sectionInfo.lineStart,
+				sectionInfo.lineEnd
+			);
+		});
+	}
+
+	/**
+	 * Fallback saving incase writing back to the timekeep block fails,
+	 * if writing back fails attempt to write to a backup temporary file
+	 * using the current date time
+	 *
+	 * @param timekeep The timekeep to save
+	 */
+	async saveFallback(timekeep: Timekeep) {
+		// Fallback incase of write failure, attempt to write to another file
+		const backupFileName = `timekeep-write-backup-${moment().format("YYYY-MM-DD HH-mm-ss")}.json`;
+
+		// Write to the backup file
+		this.app.vault.create(backupFileName, JSON.stringify(timekeep));
+	}
+}
