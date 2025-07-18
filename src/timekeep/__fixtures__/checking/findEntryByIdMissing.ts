306eb302e424daedf4c1a9194db97144be9cf85a
Jacobtread <jacobtread@gmail.com>
2025-07-18 21:28:10 +1200

    feat: remaining tests for new field coverage

---
+++ src/timekeep/__fixtures__/checking/findEntryByIdMissing.ts
@@ -0,0 +1,15 @@
+import moment from "moment";
+
+export const currentTime = moment();
+
+export const targetEntryId = "9054dee3-8c15-493b-ad31-f070e08c2699";
+
+export const input = [
+	{
+		id: "7054dee3-8c15-493b-ad31-f070e08c2699",
+		name: "Part 1",
+		startTime: currentTime,
+		endTime: currentTime,
+		subEntries: null,
+	},
+];
