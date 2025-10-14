94a1b6af410a08b4a9932d2d0f2451f80110762d
Jacobtread <jacobtread@gmail.com>
2025-10-14 15:28:27 +1300

    feat: select all on merge modal

--- src/views/timekeep-merger-modal.ts
+++ src/views/timekeep-merger-modal.ts
@@ -20,6 +20,8 @@ export class TimekeepMergerModal extends Modal {
 
 	private listContainer: HTMLElement | undefined;
 	private searchInput: TextComponent | undefined;
+	private selectAll: HTMLInputElement | undefined;
+	private selectContainer: HTMLDivElement | undefined;
 	private loadingEl: HTMLElement | undefined;
 	private exportPdf: boolean;
 	private settings: Store<TimekeepSettings>;
@@ -56,19 +58,45 @@ export class TimekeepMergerModal extends Modal {
 		this.searchInput.onChange(() => {
 			this.updateFilteredResults();
 			this.renderList();
+			this.updateSelectAll();
 		});
+
+		const selectContainer = this.contentEl.createDiv();
+		selectContainer.style.display = "none";
+		selectContainer.style.flexDirection = "row";
+		selectContainer.style.alignItems = "center";
+		selectContainer.style.gap = "0.5rem";
+		selectContainer.style.paddingLeft = "0.75rem";
+		selectContainer.style.paddingTop = "1rem";
+		selectContainer.style.fontWeight = "bold";
+		this.selectContainer = selectContainer;
+
+		const selectAll = selectContainer.createEl("input", {
+			type: "checkbox",
+		});
+		selectAll.checked = this.isAllSelected();
+		selectAll.id = `merge-select-all`;
+		selectAll.onchange = () => {
+			this.toggleSelectAll(selectAll.checked);
+			this.renderList();
+		};
+		this.selectAll = selectAll;
+
+		const selectAllLabel = selectContainer.createEl("label");
+		selectAllLabel.htmlFor = "merge-select-all";
+		selectAllLabel.textContent = "Select All";
 
 		this.listContainer = this.contentEl.createDiv();
 		this.listContainer.style.maxHeight = "400px";
 		this.listContainer.style.overflowY = "auto";
-		this.listContainer.style.marginTop = "1em";
+		this.listContainer.style.marginTop = "0.25em";
 		this.listContainer.style.padding = "0.25em";
 
 		const footer = this.contentEl.createDiv();
 		footer.style.display = "flex";
 		footer.style.flexFlow = "row";
 		footer.style.gap = "1rem";
-		footer.style.marginTop = "1rem";
+		footer.style.marginTop = "0.5rem";
 
 		const mergeButton = new ButtonComponent(footer)
 			.setButtonText("Create")
@@ -112,6 +140,7 @@ export class TimekeepMergerModal extends Modal {
 
 			this.updateFilteredResults();
 			this.renderList();
+			this.updateSelectAll();
 
 			this.loadingEl.remove();
 			mergeButton.setDisabled(false);
@@ -123,6 +152,74 @@ export class TimekeepMergerModal extends Modal {
 		}
 	}
 
+	updateSelectAll() {
+		if (this.selectContainer) {
+			this.selectContainer.style.display =
+				this.filteredResults.length > 0 ? "flex" : "none";
+		}
+
+		const isAllSelected = this.isAllSelected();
+		if (this.selectAll) {
+			this.selectAll.checked = isAllSelected;
+		}
+	}
+
+	/**
+	 * Check if the whole filtered result set is selected
+	 */
+	isAllSelected() {
+		// Must have at least one result to be selected
+		if (this.filteredResults.length < 1) {
+			return false;
+		}
+
+		for (const result of this.filteredResults) {
+			const selected = this.selectedResults.find(
+				(selected) => selected.id === result.id
+			);
+
+			if (selected === undefined) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Toggle the "select all" for the current filtered result set
+	 *
+	 * When checked adds all visible results to the current selection
+	 * When unchecked removes all visible results from the current selection
+	 *
+	 * @param checked Whether to select all
+	 */
+	toggleSelectAll(checked: boolean) {
+		if (checked) {
+			// Find all results that aren't already selected
+			const toBeSelected = this.filteredResults.filter((result) => {
+				const selected = this.selectedResults.find(
+					(selected) => selected.id === result.id
+				);
+				return selected === undefined;
+			});
+
+			this.selectedResults = [...this.selectedResults, ...toBeSelected];
+		} else {
+			// Keep only the results that aren't in the current filtered list
+			this.selectedResults = this.selectedResults.filter((selected) => {
+				const filtered = this.filteredResults.find(
+					(filtered) => filtered.id === selected.id
+				);
+				return filtered === undefined;
+			});
+		}
+	}
+
+	/**
+	 * Update the current filtered results to match the
+	 * search query
+	 */
 	updateFilteredResults() {
 		if (!this.searchInput) return;
 
@@ -137,6 +234,9 @@ export class TimekeepMergerModal extends Modal {
 		});
 	}
 
+	/**
+	 * Locate all timekeep codeblocks in files
+	 */
 	async getResults(): Promise<TimekeepResult[]> {
 		const markdownFiles = this.app.vault.getMarkdownFiles();
 		const batchSize = 25;
@@ -165,6 +265,9 @@ export class TimekeepMergerModal extends Modal {
 		return results;
 	}
 
+	/**
+	 * Render the list of filtered results
+	 */
 	private renderList() {
 		if (!this.listContainer) {
 			return;
@@ -221,5 +324,6 @@ export class TimekeepMergerModal extends Modal {
 		this.searchInput = undefined;
 		this.listContainer = undefined;
 		this.loadingEl = undefined;
+		this.selectAll = undefined;
 	}
 }
