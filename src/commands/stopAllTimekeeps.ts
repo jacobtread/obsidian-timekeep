81dbed3486ca9e39d2d9295ff62492ded848c3d6
Jacobtread <jacobtread@gmail.com>
2026-03-30 10:08:17 +1300

    refactor: cleanup async commands and fix stopAllTimekeeps command had wrong source

--- src/commands/stopAllTimekeeps.ts
+++ src/commands/stopAllTimekeeps.ts
@@ -1,45 +1,39 @@
-import moment from "moment";
-import { Notice, type App, type Command } from "obsidian";
+import type { App, Command } from "obsidian";
 
-import { stopFileTimekeeps } from "@/timekeep/stopFileTimekeeps";
+import moment from "moment";
+import { Notice } from "obsidian";
+
+import { stopAllTimekeeps } from "@/timekeep/stopAllTimekeeps";
+
+async function asyncCallback(app: App) {
+	const currentTime = moment();
+
+	try {
+		const totalStopped = await stopAllTimekeeps(app.vault, currentTime);
+		if (totalStopped < 1) {
+			new Notice("Nothing to stop.", 1500);
+			return;
+		}
+
+		new Notice(`Stopped ${totalStopped} tracker${totalStopped !== 1 ? "s" : ""}`, 1500);
+	} catch (error) {
+		let errorMessage = "";
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		} else if (typeof error === "string") {
+			errorMessage = error;
+		} else {
+			errorMessage = "Unknown error occurred";
+		}
+
+		new Notice("Failed to stop timekeeps: " + errorMessage, 1500);
+	}
+}
 
 export default function (app: App): Command {
 	return {
-		id: `stop-current-timekeeps`,
-		name: `Stop All Running Trackers (Current File Only)`,
-		callback: () => {
-			const currentTime = moment();
-			const currentFile = app.workspace.activeEditor?.file ?? null;
-
-			if (currentFile === null) {
-				new Notice("No active file detected", 1500);
-				return;
-			}
-
-			stopFileTimekeeps(app.vault, currentFile, currentTime)
-				.then((totalStopped) => {
-					if (totalStopped < 1) {
-						new Notice("Nothing to stop.", 1500);
-						return;
-					}
-
-					new Notice(
-						`Stopped ${totalStopped} tracker${totalStopped !== 1 ? "s" : ""}`,
-						1500
-					);
-				})
-				.catch((error) => {
-					let errorMessage = "";
-					if (error instanceof Error) {
-						errorMessage = error.message;
-					} else if (typeof error === "string") {
-						errorMessage = error;
-					} else {
-						error = "Unknown error occurred";
-					}
-
-					new Notice("Failed to stop timekeeps: " + errorMessage, 1500);
-				});
-		},
+		id: `stop-all-timekeeps`,
+		name: `Stop All Running Trackers`,
+		callback: () => void asyncCallback(app),
 	};
 }
--- src/commands/stopFileTimekeeps.ts
+++ src/commands/stopFileTimekeeps.ts
@@ -5,43 +5,42 @@ import { Notice } from "obsidian";
 
 import { stopFileTimekeeps } from "@/timekeep/stopFileTimekeeps";
 
+async function asyncCallback(app: App) {
+	const currentTime = moment();
+	const currentFile = app.workspace.activeEditor?.file ?? null;
+
+	if (currentFile === null) {
+		new Notice("No active file detected", 1500);
+		return;
+	}
+
+	try {
+		const totalStopped = await stopFileTimekeeps(app.vault, currentFile, currentTime);
+
+		if (totalStopped < 1) {
+			new Notice("Nothing to stop.", 1500);
+			return;
+		}
+
+		new Notice(`Stopped ${totalStopped} tracker${totalStopped !== 1 ? "s" : ""}`, 1500);
+	} catch (error) {
+		let errorMessage = "";
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		} else if (typeof error === "string") {
+			errorMessage = error;
+		} else {
+			errorMessage = "Unknown error occurred";
+		}
+
+		new Notice("Failed to stop timekeeps: " + errorMessage, 1500);
+	}
+}
+
 export default function (app: App): Command {
 	return {
 		id: `stop-current-timekeeps`,
 		name: `Stop All Running Trackers (Current File Only)`,
-		callback: () => {
-			const currentTime = moment();
-			const currentFile = app.workspace.activeEditor?.file ?? null;
-
-			if (currentFile === null) {
-				new Notice("No active file detected", 1500);
-				return;
-			}
-
-			stopFileTimekeeps(app.vault, currentFile, currentTime)
-				.then((totalStopped) => {
-					if (totalStopped < 1) {
-						new Notice("Nothing to stop.", 1500);
-						return;
-					}
-
-					new Notice(
-						`Stopped ${totalStopped} tracker${totalStopped !== 1 ? "s" : ""}`,
-						1500
-					);
-				})
-				.catch((error) => {
-					let errorMessage = "";
-					if (error instanceof Error) {
-						errorMessage = error.message;
-					} else if (typeof error === "string") {
-						errorMessage = error;
-					} else {
-						error = "Unknown error occurred";
-					}
-
-					new Notice("Failed to stop timekeeps: " + errorMessage, 1500);
-				});
-		},
+		callback: () => void asyncCallback(app),
 	};
 }
