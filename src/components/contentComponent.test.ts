bd666046975ef0cead239cdd619d14fd3f47b46c
Jacobtread <jacobtread@gmail.com>
2026-04-05 18:13:24 +1200

    tests: replaceableComponent and contentComponent tests

--- src/components/contentComponent.test.ts
+++ src/components/contentComponent.test.ts
@@ -6,6 +6,7 @@ import { createMockContainer } from "@/__mocks__/obsidian";
 
 import { ContentComponent } from "./contentComponent";
 import { DomComponent } from "./domComponent";
+import { ReplaceableComponent } from "./replaceableComponent";
 
 describe("ContentComponent", () => {
 	let container: HTMLElement;
@@ -60,5 +61,61 @@ describe("ContentComponent", () => {
 		comp.setContent(undefined);
 		expect(comp.getContent()).toBeUndefined();
 		expect(comp.containerEl!.children.length).toBe(0);
+	});
+
+	it("using two replaceable component should trigger the previous", () => {
+		class FirstComponent extends ReplaceableComponent {
+			testContainer: HTMLElement;
+
+			constructor(containerEl: HTMLElement) {
+				super(containerEl);
+			}
+
+			createContainer(): HTMLElement {
+				this.testContainer = createDiv();
+				return this.testContainer;
+			}
+
+			render(wrapperEl: HTMLElement): void {
+				wrapperEl.createSpan({ text: "First" });
+			}
+		}
+
+		class SecondComponent extends ReplaceableComponent {
+			testContainer: HTMLElement;
+
+			constructor(containerEl: HTMLElement) {
+				super(containerEl);
+			}
+
+			createContainer(): HTMLElement {
+				this.testContainer = createDiv();
+				return this.testContainer;
+			}
+
+			render(wrapperEl: HTMLElement): void {
+				wrapperEl.createSpan({ text: "Second" });
+			}
+		}
+
+		const comp = new ContentComponent<FirstComponent | SecondComponent>(container);
+		comp.load();
+
+		const firstComponent = new FirstComponent(container);
+		const secondComponent = new SecondComponent(container);
+
+		comp.setContent(firstComponent);
+		expect(container.children[0].textContent).toBe("First");
+		expect(container.children[0]).toBe(firstComponent.testContainer);
+
+		comp.setContent(secondComponent);
+
+		// Child container and text should match
+		expect(container.children[0]).toBe(secondComponent.testContainer);
+		expect(container.children[0].textContent).toBe("Second");
+
+		// First component should skip unloading
+		expect(firstComponent.isUnloadSkipped()).toBe(true);
+		expect(firstComponent.skipUnloadReplace).toBe(true);
 	});
 });
